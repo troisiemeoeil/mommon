84960bd50f2e2dea6de8b0432771f67c95b337f3
troisiemeoeil <troisiemeoeilagency@gmail.com>
2024-12-08 05:19:37 +0200

    fixed module

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
